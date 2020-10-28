c4e6015c92906104fa89579613b5558072e86ae9
SuelyChu <1021109865@qq.com>
2020-10-28 17:49:17 +0800

    加入案例: 对字符串这种的数据进行排序

--- JavaPractice/Test/src/MyTest/Test01.java
+++ JavaPractice/Test/src/MyTest/Test01.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 
 public class Test01 {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
-        String s = Arrays.toString(arr);
-        System.out.println(s);
+        int[] arr = {24, 69, 80, 57, 13};
+        System.out.println("排序前: " + Arrays.toString(arr));
+        Arrays.sort(arr);
+        System.out.println("排序后: " + Arrays.toString(arr));
     }
 }
